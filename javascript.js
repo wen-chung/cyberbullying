a83b5563c15e91bf149fd00a04dcd9b10513c548
wen-chung <a22640290@gmail.com>
2017-06-12 09:06:06 +0800

    add pictures ,game

--- javascript.js
+++ javascript.js
@@ -1,11 +1,22 @@
 // inner variables
 var canvas, ctx;
-var button;
+var button, keyMan;
 var backgroundImage;
 var spaceShip;
-var iBgShiftY = 1024;
+var iBgShiftY = 1050;
 var bDrawDialog = true;
 var iDialogPage = 1;
+
+var ManShiftX = 0;
+var Itturn = true;
+var falldown = 0;
+var jump = 20;
+
+var enemy = null; // game objects
+var enemies = [];
+var iEnemyW = 219; // enemy width
+var iEnemyH = 42; // enemy height
+var iEnemySpeed = 2; // initial enemy speed
 // -------------------------------------------------------------
 
 function scroll_list(){
@@ -147,31 +158,32 @@ function Button(x, y, w, h, state, image) {
     this.imageShift = 0;
     this.image = image;
 }
-function Stage(x, y, w, h, state, image){
+function Enemy(x, y, w, h, speed, image) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
-    this.state = state;
-    this.imageShift = 0;
+    this.speed = speed;
     this.image = image;
 }
-function People(x, y, w, h, image) {
+function People(x, y, w, h, image, live) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.image = image;
-    this.bDrag = false;
+    this.live = live;
 }
 
 // -------------------------------------------------------------
 
+// get random number between X and Y
+function getRand(x, y) {
+    return Math.floor(Math.random()*y)+x;
+}
+
 // draw functions :
 
-function clear() { // clear canvas function
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
 
 function drawDialog() { // draw dialog function
     if (bDrawDialog) {
@@ -204,9 +216,9 @@ function drawDialog() { // draw dialog function
         ctx.fillStyle = '#fff';
         if (iDialogPage == 1) {
             ctx.fillText('Game', ctx.canvas.width/2, 150);
-            ctx.font = '24px DS-Digital';
-            ctx.fillText(' test2', ctx.canvas.width/2, 250);
-            ctx.fillText(' test1', ctx.canvas.width/2, 280);
+            ctx.font = '24px ';
+            ctx.fillText(' pressed "1" to jump', ctx.canvas.width/2, 250);
+            ctx.fillText(' ', ctx.canvas.width/2, 280);
         } else if (iDialogPage == 2) {
             ctx.fillText('second', ctx.canvas.width/2, 150);
             ctx.font = '24px DS-Digital';
@@ -216,31 +228,77 @@ function drawDialog() { // draw dialog function
 }
 
 function drawScene() { // main drawScene function
-    clear(); // clear canvas
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clear canvas
 
-    // draw background
-    iBgShiftY -= 10;
-    if (iBgShiftY <= 0) {
-        iBgShiftY = 1024;
+       
+    ctx.drawImage(backgroundImage, 0 , 2000 - iBgShiftY, 768, 1024, 0, 0, 800, 600);
+ 
+    
+ 
+    if(keyMan.live == false){
+        // draw dialog
+        drawDialog();
+        // draw button
+        ctx.drawImage(button.image, 0, button.imageShift, button.w, button.h, button.x, button.y, button.w, button.h);
+        ctx.font = '22px DS-Digital';
+        ctx.fillStyle = '#ffffff';
+        ctx.fillText('start', 400, 485);
     }
-    ctx.drawImage(backgroundImage, 0 , 0 + iBgShiftY, 768, 1024, 0, 0, 800, 600);
+    else{ 
+      if( Itturn == true ){
+        ManShiftX += 5;
 
-    // draw space ship
-    //ctx.drawImage(spaceShip.image, 0, 0, spaceShip.w, spaceShip.h, spaceShip.x-128, spaceShip.y-128, spaceShip.w, spaceShip.h);
+        if( ManShiftX > (900-143)){
+          Itturn = false;
+          ManShiftX = 700;
+        }
+      }
+      else{
+        ManShiftX -= 5;
 
-    // draw dialog
-    drawDialog();
+        if( ManShiftX < 10){
+          Itturn = true;
+          ManShiftX = 0;
+        }
+      }
 
-    // draw button
-    ctx.drawImage(button.image, 0, button.imageShift, button.w, button.h, button.x, button.y, button.w, button.h);
+      ctx.drawImage(keyMan.image, ManShiftX, keyMan.y + falldown);
 
-    // draw button's text
-    //ctx.font = '22px DS-Digital';
-    //ctx.fillStyle = '#ffffff';
-    //ctx.fillText('next/hide/show', 400, 465);
-    //ctx.fillText('dialog', 400, 500);
+     
+      if (enemies.length > 0) {
+        for (var ekey in enemies) {
+            if (enemies[ekey] != undefined) {
+                ctx.drawImage(enemies[ekey].image, enemies[ekey].x, enemies[ekey].y);
+
+                if (enemies[ekey].y < 0) {
+                    delete enemies[ekey];
+
+                }
+            }
+        }
+      }
+
+
+    }
 }
 
+
+function down(){
+  for (var ekey in enemies) {
+    if ((enemies[ekey].height-keyMan.y)<5) {
+          falldown = 0;
+        }
+        else
+        {
+          falldown = 30;
+        }
+    }
+}
+
+function collesion(){
+
+
+}
 // -------------------------------------------------------------
 
 
@@ -254,7 +312,7 @@ $(function(){   //其作用和 $(document).ready()一樣 ，用意在DOM載入�
 
     // load background image
     backgroundImage = new Image();
-    backgroundImage.src = 'picture/stars.jpg';
+    backgroundImage.src = 'picture/nsky.png';
     backgroundImage.onload = function() {
     }
     backgroundImage.onerror = function() {
@@ -263,12 +321,30 @@ $(function(){   //其作用和 $(document).ready()一樣 ，用意在DOM載入�
 	
 	
 	
-	// load the button sprite image
+	  // load the button sprite image
     var buttonImage = new Image();
-    buttonImage.src = 'picture/button.png';
+    buttonImage.src = 'picture/btn.png';
     buttonImage.onload = function() {
     }
     button = new Button(310, 450, 180, 120, 'begin', buttonImage);
+
+
+
+    // initialization of empty enemy
+    var oEnemyImage = new Image();
+    oEnemyImage.src = 'picture/stagee.png';
+    oEnemyImage.onload = function() { }
+
+
+    // initialization of empty keyMan
+    var oPImage = new Image();
+    oPImage.src = 'picture/r_run.png';
+    oPImage.onload = function() { }
+
+    keyMan = new People(0, canvas.height-72, 58, 72, oPImage, false);
+
+   
+    
 
     $('#scene').mousedown(function(e) { // binding mousedown event (for dragging)
 
@@ -287,49 +363,66 @@ $(function(){   //其作用和 $(document).ready()一樣 ，用意在DOM載入�
         // button behavior
         if (mouseX > button.x && mouseX < button.x+button.w && mouseY > button.y && mouseY < button.y+button.h) {
             button.state = 'pressed';
-        }
-    });
-
-    $('#scene').mousemove(function(e) { // binding mousemove event
-        var mouseX = e.layerX || 0;
-        var mouseY = e.layerY || 0;
-
-        if (!bDrawDialog && spaceShip.bDrag) {
-            spaceShip.x = mouseX;
-            spaceShip.y = mouseY;
-        }
-
-        // button behavior
-
-
-        if (button.state != 'pressed') {
-            button.imageShift = 0;
-            if (mouseX > button.x && mouseX < button.x+button.w && mouseY > button.y && mouseY < button.y+button.h) {
-                button.state = 'hover';
-                button.imageShift = 131;
-            }
-        }else{
-
+            iDialogPage = 2;          // 呈現第幾頁
+            bDrawDialog = false;
+            button.imageShift = 262;
+            keyMan.live  = true;
         }
 
 
     });
 
-    $('#scene').mouseup(function(e) { // binding mouseup event
-        spaceShip.bDrag = false;
-        // button behavior
-        if (button.state == 'pressed') {
-              //iDialogPage = 1;           呈現第幾頁
-              bDrawDialog = !bDrawDialog;
-              button.state = 'disappe';
-              button.imageShift = 0;
+
+    $(window).keydown(function(event){ // keyboard alerts
+        switch (event.keyCode) {
+            case 49: // '1' key
+                if(iBgShiftY<2000){
+                  iBgShiftY += 30;
+
+                  keyMan = new People(keyMan.x, keyMan.y-30, 58, 72, oPImage, true);
+                  down();
+                  ctx.drawImage(backgroundImage, 0 , background.height - 100, 768, 1024, 0, 0, 800, 600);
+                  if (enemies.length > 0) {
+                    for (var ekey in enemies) {
+                      if (enemies[ekey] != undefined) {
+                          ctx.drawImage(enemies[ekey].image, enemies[ekey].x, enemies[ekey].y);
+                            enemies[ekey].y -= enemies[ekey].speed;
+
+                        if (enemies[ekey].y < - iEnemyH) {
+                          delete enemies[ekey];
+                        }
+                      } 
+                    }
+                  }
+                }
+                break;
         }
     });
-
+    
 
 
     scroll_list();
     setInterval(drawScene, 30); // loop drawScene
+
+   
+   var enTimer = null;
+    function addEnemy() {
+        clearInterval(enTimer);   //?秒新增
+        var randX = getRand(0, canvas.width - iEnemyW);
+        var randY = getRand(30, 2000-42);
+        enemies.push(new Enemy(randX, randY, iEnemyW, iEnemyH, - iEnemySpeed, oEnemyImage));
+      
+        var interval = getRand(5000, 10000);
+        enTimer = setInterval(addEnemy, interval); // loop drawScene
+    }
+    addEnemy();
+
+
+
+
+
+
+
 });
 
 
